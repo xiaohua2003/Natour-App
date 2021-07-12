5aa3cb18a6e263b83c073b5e997a313c017ab294
xiaohua2003 <xcai3@ualberta.ca>
2021-07-11 18:19:30 -0600

    update server.js and config.env

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-
-const dotenv=require('dotenv')
-dotenv.config({path:'./config.env'})
-const app = require('./app')
-console.log(process.env)
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+//read variable from files and save them as environment variables
+dotenv.config({path:'./config.env'});
+const app = require('./app');
+const DB=process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB,{useNewUrlParser:true, 
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(con=>{
+    
+    console.log('connection was successful')
+});
 //start server
-const port = 3000||process.env.PORT
+const port = 5000||process.env.PORT
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
